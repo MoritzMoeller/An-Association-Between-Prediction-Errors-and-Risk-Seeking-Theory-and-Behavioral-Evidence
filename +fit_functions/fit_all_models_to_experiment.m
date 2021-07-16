7ff88e4ad4a0f53d56907800e4e74c231a794bcd
Jan <jangrohn@gmail.com>
2021-07-16 13:36:44 +0100

    changed model fit notation

--- +fit_functions/fit_all_models_to_experiment.m
+++ +fit_functions/fit_all_models_to_experiment.m
@@ -5,6 +5,6 @@ MODELS = helper_functions.get_all_models();
 %%
 
 parfor im = 1:length(MODELS)
-        fit_model('plosCB2021', MODELS)
+        fit_functions.fit_model('plosCB2021', MODELS{im})
 end
 end
